d27bcdd86aa732281d44ee093a11395c9d94e9d1
Paulo França <paulofranca.contato@gmail.com>
2018-06-09 15:49:42 -0300

    Inserindo novo Cliente com verbo POST

--- src/main/java/br/com/paulofranca/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/paulofranca/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.paulofranca.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.paulofranca.cursomc.dto.ClienteDTO;
+import br.com.paulofranca.cursomc.dto.ClienteNewDTO;
 import br.com.paulofranca.cursomc.model.Cliente;
 import br.com.paulofranca.cursomc.services.ClienteService;
 
@@ -27,18 +31,28 @@ public class ClienteResource {
 
 	@Autowired
 	private ClienteService service;
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = this.service.find(id);
 
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO clienteNewDto) {
+		Cliente cliente = this.service.fromDTO(clienteNewDto);
+
+		cliente = this.service.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = this.service.fromDTO(objDto);
-		
+
 		obj.setId(id);
 		obj = this.service.update(obj);
 
