138264729cae743915ddbbb8dd668cac742d2aea
Paulo França <paulofranca.contato@gmail.com>
2018-06-09 13:46:57 -0300

    personalizando o retorno da validação

--- src/main/java/br/com/paulofranca/cursomc/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/br/com/paulofranca/cursomc/resources/exception/ResouceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +27,19 @@ public class ResouceExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Error de validação", System.currentTimeMillis());
+		
+		// pecorrer lista de errors
+		for (FieldError fiedError : e.getBindingResult().getFieldErrors()) {
+			error.addError(fiedError.getField(), fiedError.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
