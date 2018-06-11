4c643dc4a081ac329f8cf61fdb4ef00f94e24f88
Paulo França <paulofranca.contato@gmail.com>
2018-06-10 22:19:28 -0300

    valicao customizado: email não repetido na atualização de Cliente

--- src/main/java/br/com/paulofranca/cursomc/dto/ClienteDTO.java
+++ src/main/java/br/com/paulofranca/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.paulofranca.cursomc.model.Cliente;
+import br.com.paulofranca.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ src/main/java/br/com/paulofranca/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,55 @@
+package br.com.paulofranca.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.paulofranca.cursomc.dto.ClienteDTO;
+import br.com.paulofranca.cursomc.model.Cliente;
+import br.com.paulofranca.cursomc.model.enums.TipoCliente;
+import br.com.paulofranca.cursomc.repositories.ClienteRepository;
+import br.com.paulofranca.cursomc.resources.exception.FieldMessage;
+import br.com.paulofranca.cursomc.services.validation.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private ClienteRepository repository;
+
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		// pegando parametro na URI ex: http://localhost:8080/clientes/{2}
+		// (atributos/parametros armazenados em um Map)
+		Map<String, String> map = (Map<String, String>) this.request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id")); // ID do meu cliente para update
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente aux = this.repository.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
